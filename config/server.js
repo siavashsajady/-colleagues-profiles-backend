9d522d4b4c9a42d3c32fc429f73d8b7fd301af97
siavashsajady <hubgitpro@gmail.com>
2022-01-08 14:33:44 +0100

    Setup Strapi CMS to create full API

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd45ff9e4d7f99116714d344b661e13cd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4fbd6b2-14d6-44c5-91e3-a64743cc7ffb'
+};
